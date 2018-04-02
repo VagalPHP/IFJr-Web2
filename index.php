4511eae5d86b605541a44b1b00c5a806fe6d928d
matheus-gio <matheus.giovanni.lu@gmail.com>
2018-04-02 20:17:47 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <meta charset="utf-8">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/freelancer.css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-expand-md navbar-custom bg-custom">
+    <div class="container">
+      <a class="navbar-brand" href="?pag=index">IFJR</a>
+  <button class="navbar-toggler btn btn-dark" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    Menu
+  </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+
+          <li class="nav-item">
+            <a class="nav-link" href="?pag=index#about">Empresa <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?pag=quem-somos#conteudo">Sobre nós</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?pag=index#contact">Fale Conosco</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+    </nav>
+   
+  <header class="video-hero" id="index">
+
+      <div class="video-bg demo-video">
+          <video autoplay muted loop poster="img/typing-macbook.jpg">
+              <source src="video/typing-macbook.mp4" type="video/mp4">
+          </video>
+      </div>
+      <div class="video-overlay"></div>
+      <div class="page-width">
+          <div class="video-hero--content">
+              <div class="intro-text">
+                  <span class="name" > 
+                    <div class="col-md-12">
+                      <img class="img-fluid" src="img/logo.png" alt="">
+                        <!--    <p style= "font-size: 140%; text-align: center"> Inovando seus sonhos</p>  --><br>
+                        <a href="?pag=index#contact" class="btn btn-lg btn-outline">
+                        <i class="fa fa-users" aria-hidden="true"></i> Faça seu contato
+                      </a>
+                    </div>
+                  </span>
+
+                  <span class="skills"></span>
+              </div>
+          </div>
+      </div>
+
+  </header>
+
+
+<section class="success" id="about">
+  <div class="row">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+            <h2>Diferencial</h2>
+            <br><br>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+          <center>One of three columns</center>
+
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4"">
+          <center>One of three columns</center>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4"">
+          <center>One of three columns</center>
+        </div>
+      </div>
+      <br>
+    </div>
+  </div>
+</section>
+
+
+
+    <back
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="jquery/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
